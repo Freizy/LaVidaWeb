c91a4b5251892427f09dd580594970990d979994
Freizy <61235054+Freizy@users.noreply.github.com>
2023-01-12 16:23:41 +0000

    LaVida v1.0.0

---
+++ pages/api/api.js
@@ -0,0 +1,20 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const { gender, age, symptoms } = req.body;
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: generatePrompt( gender, age, symptoms ),
+    temperature: 0.7,
+    max_tokens: 3648,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+function generatePrompt(gender, age, symptoms) {
+  return `briefly explain 5 sicknesses, its causes and solutions a ${age} years old ${gender} experiencing ${symptoms} may be having.`;
+}
